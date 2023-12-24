d627067bf1a911f94fbafc5efb33b98b0e86cf6b
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-12-23 21:25:27 -0300

    feito: variaveis e constantes

---
+++ src/main/variaveis/variaveis01.js
@@ -0,0 +1,7 @@
+console.log("Digite o nome do seu jogador: ")
+
+let nickname = "Jim Carrey"
+
+console.log("Bem vindo " + nickname)
+
+console.log(nickname + " entrou no servidor!")
---
+++ src/main/variaveis/variaveis02.js
@@ -0,0 +1,22 @@
+let poteCafe = "Café Catuaí Vermelho"
+let poteAcuar = "Açúcar Cristal"
+let poteBiscoito = "Biscoito Maizena"
+
+const mensagemDaVovo = "No cozinha da Vovó hoje tem "
+
+
+console.log(mensagemDaVovo
+    + poteCafe + ", "
+    + poteAcuar + " e "
+    + poteBiscoito
+)
+
+poteCafe = "Café Bourbon Amarelo"
+poteAcuar = "Açúcar Mascavo"
+poteBiscoito = "Biscoito Sequilhos"
+
+console.log(mensagemDaVovo
+    + poteCafe + ", "
+    + poteAcuar + " e "
+    + poteBiscoito
+)
