a920e93166021cfd80a4d63669950c2fed2de0c3
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2024-05-09 16:21:44 -0300

    inicio de collections

---
+++ src/main/colecoes/00.js
@@ -0,0 +1,10 @@
+// vetor
+let nomePokemon = ["Pikachu", "Charmander", "Bulbasaur"]
+//console.log(nomePokemon[0])
+
+// matriz
+let timePokemon = [
+    ["Charizard", 64, 58],
+    ["Charmander", 52, 39]
+]
+console.log("Pokemon " + timePokemon[0][0] + " tem de Ataque: " + timePokemon[0][1] + " e HP " + timePokemon[0][2])
